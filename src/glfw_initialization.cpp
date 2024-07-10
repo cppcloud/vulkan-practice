2bcd457504158547668fdd44a147c765d6ce1160
cppcloud <admin@cplusplus.cloud>
2024-07-10 13:15:39 +0800

    vulkan

---
+++ src/glfw_initialization.cpp
@@ -0,0 +1,24 @@
+#include <precomp.h>
+#include <GLFW/glfw3.h>
+#include <glfw_initialization.h>
+#include <spdlog/spdlog.h>
+
+namespace veng {
+
+void glfw_error_callback(std::int32_t error_code, gsl::czstring message) {
+  spdlog::error("Glfw Validation: {}", message);
+}
+
+GlfwInitialization::GlfwInitialization() {
+  glfwSetErrorCallback(glfw_error_callback);
+
+  if (glfwInit() != GLFW_TRUE) {
+    std::exit(EXIT_FAILURE);
+  }
+}
+
+GlfwInitialization::~GlfwInitialization() {
+  glfwTerminate();
+}
+
+}  // namespace veng
---
+++ src/precomp.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <gsl/gsl>
+#include <string>
+#include <string_view>
+#include <glm/glm.hpp>
+#include <utilities.h>
+#include <functional>
+#include <optional>
